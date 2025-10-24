199a0c2b0dda8ab14ec7687c02352e2f7d737948
DEveloper <mistrybhavesh1997@gmail.com>
2025-10-24 09:44:23 +0530

    remove add mob

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import google_mobile_ads
 import FirebaseCore
 
 
@@ -14,12 +13,6 @@ import FirebaseCore
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
       // Register NativeAdFactory
-        let factory = NativeAdFactoryExample()
-        FLTGoogleMobileAdsPlugin.registerNativeAdFactory(
-          self,
-          factoryId: "listTile",
-          nativeAdFactory: factory
-        )
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
