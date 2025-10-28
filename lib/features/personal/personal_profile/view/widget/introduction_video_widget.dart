926decf5c4465d69e91103e6fa562e79c9448618
Prabharamaraj <prabharamaraj591@gmail.com>
2025-10-28 18:41:29 +0530

    bugs fixxing

--- lib/features/personal/personal_profile/view/widget/introduction_video_widget.dart
+++ lib/features/personal/personal_profile/view/widget/introduction_video_widget.dart
@@ -13,6 +13,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:video_player/video_player.dart';
 
+import '../../../../../core/constants/app_icon_assets.dart';
+import '../../../../../widgets/local_assets.dart';
+
 class IntroductionVideoWidget extends StatefulWidget {
   const IntroductionVideoWidget({Key? key}) : super(key: key);
 
@@ -60,20 +63,29 @@ class _IntroductionVideoWidgetState extends State<IntroductionVideoWidget> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             /// Title
-            CustomText(
-              'Upload Introduction Video',
-              fontSize: SizeConfig.medium,
-              fontWeight: FontWeight.w600,
-              color: AppColors.secondaryTextColor,
+            Row(
+              children: [
+                _buildCircleIcon(AppIconAssets.video_outline),
+                SizedBox(width: SizeConfig.size6),
+
+
+                CustomText(
+                  'Upload Introduction Video',
+                  fontSize: SizeConfig.medium,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.secondaryTextColor,
+                ),
+              ],
             ),
             SizedBox(height: SizeConfig.size12),
 
             // Show different UI based on whether video is uploaded or not
             introVideoController.hasUploadedVideo.value
                 ? _buildVideoPreview()
-                : _buildUploadArea(),
+                : Container(),
+            //_buildUploadArea(),
 
-            SizedBox(height: SizeConfig.size16),
+            SizedBox(height: SizeConfig.size6),
 
             // Show upload button if no video is uploaded, otherwise show edit button
             introVideoController.hasUploadedVideo.value
@@ -94,20 +106,47 @@ class _IntroductionVideoWidgetState extends State<IntroductionVideoWidget> {
                       ),
                     ],
                   )
+
                 : PositiveCustomBtn(
+              isShadowAvailable: true,
+              bgColor: AppColors.white,
+              leadingIconColor:AppColors.grayText,
+              isLeadingShow: true,
+              textColor: AppColors.grayText,
+              borderColor: AppColors.whiteE5,
+              leadingIconPath:AppIconAssets.uploadIcon,
                     onTap: () async {
-                      if (introVideoController.selectedVideo.value != null) {
+                      // if (introVideoController.selectedVideo.value != null) {
+                      //
+                      // } else {
+                        await _pickVideo();
                         await introVideoController.uploadIntroVideo(videoLink: '');
-                      } else {
-                        commonSnackBar(message: "Please select a video first");
-                      }
+                        // commonSnackBar(message: "Please select a video first");
+                      // }
                     },
-                    title: introVideoController.isUploading.value ? "Uploading..." : "Upload",
+                    title: introVideoController.isUploading.value ? "Uploading..." : "Upload Video",
                   )
           ],
         ),
       );
     });
+
+  }
+  Widget _buildCircleIcon(String iconImage) {
+    return Container(
+      padding: EdgeInsets.all(SizeConfig.size6),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: AppColors.primaryColor.withValues(alpha: 0.1),
+        border: Border.all(color: AppColors.primaryColor, width: 0.5),
+      ),
+      child: LocalAssets(
+        width: SizeConfig.size22,
+        height: SizeConfig.size22,
+        imagePath: iconImage,
+        imgColor: AppColors.primaryColor,
+      ),
+    );
   }
 
   Widget _buildUploadArea() {
