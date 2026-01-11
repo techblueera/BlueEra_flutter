9cdb8ab92198ed0ef56562e91f5fcffe471e4a1e
Prabharamaraj <prabharamaraj591@gmail.com>
2026-01-11 19:44:33 +0530

    base changes

--- lib/core/api/apiService/base_service.dart
+++ lib/core/api/apiService/base_service.dart
@@ -583,15 +583,10 @@ abstract class BaseService {
   final String createHotelService = 'hotel-service/api/businesses';
   final String hotelBulkStatus = 'hotel-service/api/hotels//offerings/bulk-toggle';
   final String hotelBulkCatalogStatus = 'hotel-service/api/businesses/hotels//catalog';
-  final String hotelBulkStatus = 'hotel-service/api/hotels/offerings/bulk-toggle';
-  final String hotelBulkCatalogStatus = 'hotel-service/api/businesses/hotels/catalog';
-  final String schoolUserID = 'education-service/schools/';
-  // final String hotelBulkStatus = 'hotel-service/api/hotels//offerings/bulk-toggle';
-  // final String hotelBulkCatalogStatus = 'hotel-service/api/businesses/hotels//catalog';
-  final String saveHospitalAiDetails = 'health-service/api/businesses/ai-hospital/offerings/save';
   // final String hotelBulkStatus = 'hotel-service/api/hotels/offerings/bulk-toggle';
   // final String hotelBulkCatalogStatus = 'hotel-service/api/businesses/hotels/catalog';
   final String schoolUserID = 'education-service/schools/';
+  final String saveHospitalAiDetails = 'health-service/api/businesses/ai-hospital/offerings/save';
   final String myGroceryOrders = 'rider-service/grocery/orders/business';
   final String groceryOrderItemAvailability = 'rider-service/grocery/orders/item-availability';
   final String groceryOrderAvailableItem = 'rider-service/grocery/orders/available-items';
