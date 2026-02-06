82f1e069add3613b460c6c7310433a7c6efdc705
Prabharamaraj <prabharamaraj591@gmail.com>
2026-02-06 19:03:30 +0530

    conflict changes

--- lib/features/common/bottomNavigationBar/view/bottom_navigation_bar_screen.dart
+++ lib/features/common/bottomNavigationBar/view/bottom_navigation_bar_screen.dart
@@ -119,9 +119,6 @@ class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
     // _fetchAllAdminVideos();
   }
 
-  void _fetchAllAdminVideos() {
-    bottomBarController.fetchAllAdminVideoApi();
-  }
 
   Future<void> checkByRiderCall() async {
     String? orderId = await getCurrentCall();
