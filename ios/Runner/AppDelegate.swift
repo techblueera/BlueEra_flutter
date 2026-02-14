2231ee3292dc50c3f641be527de00f21f202dcb0
DEveloper <mistrybhavesh1997@gmail.com>
2026-02-14 20:49:26 +0530

    ios issue fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import FirebaseCore
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import FirebaseCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDdU2Ji6dCQ4Hq0TbLHILxMsdR-M27Ie2g")
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
       // Register NativeAdFactory
