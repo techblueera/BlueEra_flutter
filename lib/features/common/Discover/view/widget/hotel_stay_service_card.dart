36a4f197409ed439d0414fe7d879165770ef15b4
Himanshu sharma <himanshusharmars129@gmail.com>
2026-04-07 17:39:05 +0530

    commit code after making  discover screen new tabs

--- lib/features/common/Discover/view/widget/hotel_stay_service_card.dart
+++ lib/features/common/Discover/view/widget/hotel_stay_service_card.dart
@@ -12,7 +12,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HotelStayServiceCard extends StatelessWidget {
-  const HotelStayServiceCard({super.key});
+  final bool isShowInGrid;
+
+  const HotelStayServiceCard({super.key, required this.isShowInGrid});
 
   @override
   Widget build(BuildContext context) {
@@ -34,74 +36,150 @@ class HotelStayServiceCard extends StatelessWidget {
             ],
           ),
           SizedBox(height: SizeConfig.paddingXSL),
-          SizedBox(
-            height: SizeConfig.size180,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              padding: EdgeInsets.symmetric(horizontal: SizeConfig.size12),
-              itemCount: stayItemsCategories.length,
-              itemBuilder: (context, index) {
-                final item = stayItemsCategories[index];
-                return InkWell(
-                  onTap: () {
-                    Get.to(() => AllStayServiceScreen(
-                        stayCategories: stayItemsCategories,
-                        selectedStayCategory: item));
-                  },
-                  child: Container(
-                    width: SizeConfig.size150,
-                    margin: EdgeInsets.only(right: SizeConfig.size12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: const Color(0xffDDE2EE), width: 1.0),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Column(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Container(
-                              width: double.infinity,
-                              color: item.colorCode,
-                              child: LocalAssets(imagePath: item.icon ?? ""),
-                            ),
+          isShowInGrid ? SizedBox(
+          height: SizeConfig.size180,
+        child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+    padding: EdgeInsets.symmetric(horizontal: SizeConfig.size12),
+    itemCount: stayItemsCategories.length,
+    itemBuilder: (context, index) {
+    final item = stayItemsCategories[index];
+    return InkWell(
+    onTap: () {
+    Get.to(() => AllStayServiceScreen(
+    stayCategories: stayItemsCategories,
+    selectedStayCategory: item));
+    },
+    child: Container(
+    width: SizeConfig.size150,
+    margin: EdgeInsets.only(right: SizeConfig.size12),
+    decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(20),
+    border: Border.all(color: const Color(0xffDDE2EE), width: 1.0),
+    ),
+    child: ClipRRect(
+    borderRadius: BorderRadius.circular(20),
+    child: Column(
+    children: [
+    Expanded(
+    flex: 3,
+    child: Container(
+    width: double.infinity,
+    color: item.colorCode,
+    child: LocalAssets(imagePath: item.icon ?? ""),
+    ),
+    ),
+    Container(
+    width: double.infinity,
+    color: AppColors.white,
+    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+    child: Column(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+    CustomText(
+    item.name,
+    textAlign: TextAlign.center,
+    color: AppColors.mainTextColor,
+    fontWeight: FontWeight.w500,
+    maxLines: 2,
+    overflow: TextOverflow.ellipsis,
+    ),
+    if (item.subtitle != null) ...[
+    const SizedBox(height: 2),
+    CustomText(
+    item.subtitle ?? "N/A",
+    fontSize: SizeConfig.small,
+    color: AppColors.secondaryTextColor,
+    maxLines: 1,
+    overflow: TextOverflow.ellipsis,
+    ),
+    ],
+    ],
+    ),
+    ),
+    ],
+    ),
+    ),
+    ),
+    );
+    },
+    ),
+    ) : Padding(
+            padding: const EdgeInsets.symmetric(horizontal:18.0),
+            child: LayoutBuilder(builder: (context, constraints) {
+              const double spacing = 10;
+              const int columns = 2;
+              final double itemWidth =
+                  (constraints.maxWidth - spacing * (columns - 1)) / columns;
+              return Wrap(
+                spacing: spacing,
+                runSpacing: spacing,
+                children: stayItemsCategories.map((item) {
+                  return SizedBox(
+                    width: itemWidth,
+                    child: InkWell(
+                      onTap: () {
+                        Get.to(() => AllStayServiceScreen(
+                            stayCategories: stayItemsCategories,
+                            selectedStayCategory: item));
+                      },
+                      child: AspectRatio(
+                        aspectRatio: 1.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(color: const Color(0xffDDE2EE), width: 1.0),
                           ),
-                          Container(
-                            width: double.infinity,
-                            color: AppColors.white,
-                            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
                             child: Column(
-                              mainAxisSize: MainAxisSize.min,
                               children: [
-                                CustomText(
-                                  item.name,
-                                  textAlign: TextAlign.center,
-                                  color: AppColors.mainTextColor,
-                                  fontWeight: FontWeight.w500,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                if (item.subtitle != null) ...[
-                                  const SizedBox(height: 2),
-                                  CustomText(
-                                    item.subtitle ?? "N/A",
-                                    fontSize: SizeConfig.small,
-                                    color: AppColors.secondaryTextColor,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
+                                Expanded(
+                                  flex: 3,
+                                  child: Container(
+                                    width: double.infinity,
+                                    color: item.colorCode,
+                                    child: LocalAssets(imagePath: item.icon ?? ""),
                                   ),
-                                ],
+                                ),
+                                Container(
+                                  width: double.infinity,
+                                  color: AppColors.white,
+                                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      CustomText(
+                                        item.name,
+                                        textAlign: TextAlign.center,
+                                        color: AppColors.mainTextColor,
+                                        fontWeight: FontWeight.w500,
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                      if (item.subtitle != null) ...[
+                                        const SizedBox(height: 2),
+                                        CustomText(
+                                          item.subtitle ?? "N/A",
+                                          fontSize: SizeConfig.small,
+                                          color: AppColors.secondaryTextColor,
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ],
+                                    ],
+                                  ),
+                                ),
                               ],
                             ),
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
+                  );
+                }).toList(),
+              );
+            }),
           ),
         ],
       ),
